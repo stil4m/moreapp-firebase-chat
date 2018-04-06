2c515d3c7278c22461b14a29fedd1a15a16e67e7
Mats Stijlaart <m.stijlaart@avisi.nl>
2018-04-06 17:01:27 +0200

    Add createRoom function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,26 @@
 import * as functions from 'firebase-functions';
 
+import * as admin from 'firebase-admin';
+admin.initializeApp(functions.config().firebase);
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const createRoom = functions.https.onRequest((request, response) => {
+   const roomName = request.query.roomName;
+   if (!roomName) {
+     response.status(400);
+     response.json({
+       success: false,
+       message: "'roomName' query param not provided"
+     })
+   }
+   return admin.firestore().collection('rooms').add({name: 'roomName', messages : [{alias : 'avisi-chat', message: 'This is a new room!'}]}).then(writeResult => {
+      response.json({
+        success: true,
+        message: 'Created room!'
+      })
+   });
+
+
+});
